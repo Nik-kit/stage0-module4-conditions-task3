4b5aa3bfeb24b08d1222e541d567b0e6e891de82
Nik-kit <nikon12344@mail.com>
2023-03-25 23:13:52 +0400

    It's alive!

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if(character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' || character == 'y' ||
+                character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U' || character == 'Y') {
+            System.out.println("Vowel");
+        } else if( character == 'b' || character == 'c' || character == 'd' || character == 'f' || character == 'g' || character == 'h'
+                || character == 'j' || character == 'k' || character == 'l' || character == 'm' || character == 'n'
+                || character == 'p' || character == 'q' || character == 'r' || character == 's' || character == 't'  || character == 'v'
+                || character == 'w' || character == 'x' ||  character == 'z' ||
+                character == 'B' || character == 'C' || character == 'D' || character == 'F' || character == 'G' || character == 'H'
+                || character == 'J' || character == 'K' || character == 'L' || character == 'M' || character == 'N'
+                || character == 'P' || character == 'Q' || character == 'R' || character == 'S' || character == 'T'  || character == 'V'
+                || character == 'W' || character == 'X' || character == 'Z') {
+            System.out.println("Consonant");
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -1,6 +1,20 @@
 package school.mjc.stage0.conditions.task3;
 
+import java.util.regex.Pattern;
+
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        if(symbol == 'a' || symbol == 'b' || symbol == 'c' || symbol == 'd' || symbol == 'e' || symbol == 'f' || symbol == 'g' || symbol == 'h'
+                || symbol == 'i' || symbol == 'j' || symbol == 'k' || symbol == 'l' || symbol == 'm' || symbol == 'n' || symbol == 'o'
+                || symbol == 'p' || symbol == 'q' || symbol == 'r' || symbol == 's' || symbol == 't' || symbol == 'u' || symbol == 'v'
+                || symbol == 'w' || symbol == 'x' || symbol == 'y' || symbol == 'z' ||
+        symbol == 'A' || symbol == 'B' || symbol == 'C' || symbol == 'D' || symbol == 'E' || symbol == 'F' || symbol == 'G' || symbol == 'H'
+                || symbol == 'I' || symbol == 'J' || symbol == 'K' || symbol == 'L' || symbol == 'M' || symbol == 'N' || symbol == 'O'
+                || symbol == 'P' || symbol == 'Q' || symbol == 'R' || symbol == 'S' || symbol == 'T' || symbol == 'U' || symbol == 'V'
+                || symbol == 'W' || symbol == 'X' || symbol == 'Y' || symbol == 'Z'){
+            System.out.println("English");
+        } else {
+            System.out.println("Non English");
+        }
     }
 }
